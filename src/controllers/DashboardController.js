7ccfe8da08e597ef5893f450f5ac8282d2e8ca02
Dieisileo <dieisileo@gmal.com>
2021-10-21 19:12:18 -0300

    banco de dados

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -3,10 +3,9 @@ const Profile = require('../model/Profile')
 const JobUtils = require('../utils/JobUtils')
 
 module.exports = {
-  index(req, res) {
-
-    const jobs = Job.get();
-    const profile = Profile.get();
+  async index(req, res) {
+    const jobs = Job.get()
+    const profile = await Profile.get()
 
     let statusCount = {
       progress: 0,
@@ -17,35 +16,43 @@ module.exports = {
     // total de horas por dia de cada job em progress
     let jobTotalHours = 0
 
-    const updatedJobs = jobs.map((job) => {
+    const updatedJobs = jobs.map(job => {
       // ajustes no job
       const remaining = JobUtils.remainingDays(job)
-      const status = remaining <= 0 ? 'done' : 'progress';
+      const status = remaining <= 0 ? 'done' : 'progress'
 
-      // somando a quantidade de status  
-      statusCount[status] += 1;
+      // somando a quantidade de status
+      statusCount[status] += 1
 
       // total de horas por dia de cada job em progress
-      jobTotalHours = status == 'progress' ? jobTotalHours + Number(job["daily-hours"]) : jobTotalHours
+      jobTotalHours =
+        status == 'progress'
+          ? jobTotalHours + Number(job['daily-hours'])
+          : jobTotalHours
 
       // OU
-       // if (status == 'progress') {
-        // jobTotalHours += Number(job["daily-hours"]) 
+      // if (status == 'progress') {
+      // jobTotalHours += Number(job["daily-hours"])
       // }
 
       return {
         ...job,
         remaining,
         status,
-        budget: JobUtils.calculateBudget(job, profile["value-hour"])
+        budget: JobUtils.calculateBudget(job, profile['value-hour'])
       }
     })
 
     // qtd de horas que quero trabalhar por dia (PROFILE)
-    // MENOS 
+    // MENOS
     // qtd de horas de cada job em progress
-    const freeHours = profile["hours-per-day"] - jobTotalHours;
+    const freeHours = profile['hours-per-day'] - jobTotalHours
 
-    return res.render("index", { jobs: updatedJobs, profile: profile, statusCount: statusCount, freeHours: freeHours })
+    return res.render('index', {
+      jobs: updatedJobs,
+      profile: profile,
+      statusCount: statusCount,
+      freeHours: freeHours
+    })
   }
 }
